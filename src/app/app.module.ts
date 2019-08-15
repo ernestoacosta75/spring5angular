95647f2e8fe256a8cc787148b859d6722bbb922b
Ernesto Antonio Rodriguez Acosta <ernestoacostacuba@localhost.localdomain>
2019-08-15 18:11:28 +0200

    DirectiveComponent created. Using *ngFor directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { DirectiveComponent } from './components/directive/directive.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    DirectiveComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/directive/directive.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directive',
+  templateUrl: './directive.component.html',
+  styleUrls: ['./directive.component.css']
+})
+export class DirectiveComponent implements OnInit {
+
+  coursesList: string [] = ['Typescript', 'Javascript', 'Java SE', 'C#', 'PHP'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
